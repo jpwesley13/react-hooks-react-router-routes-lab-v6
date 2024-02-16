9593fe4f54917121811125c7980d543ccc5d64be
Jared Patterson <jpwesley34@gmail.com>
2024-02-15 17:39:45 -0800

    Passes tests.

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+    .then(res => res.json())
+    .then(setDirectors)
+  }, [])
+
+  const directorList = directors.map(director => (
+    
+    <article key={director.id}>
+      <h2>{director.name}</h2>
+      <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+    </article>
+  )) //This is the exact same structure as Actors, so it probably could be its own component that the two of them use.
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorList}
       </main>
     </>
   );
